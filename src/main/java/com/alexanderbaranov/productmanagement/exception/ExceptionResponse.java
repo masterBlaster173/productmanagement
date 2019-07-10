6f6782681427cec26c1eeef95c6f2c52aefe12c9
truder173 <uln-al-bar@yandex.ru>
2019-07-10 11:31:27 +0400

    editing ExceptionResponse class

--- src/main/java/com/alexanderbaranov/productmanagement/exception/ExceptionResponse.java
+++ src/main/java/com/alexanderbaranov/productmanagement/exception/ExceptionResponse.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 
 public class ExceptionResponse {
+
   @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MMM-dd HH:mm:ss z")
   private Date timestamp;
   private String message;
-
 
   public ExceptionResponse(Date timestamp, String message) {
     super();
